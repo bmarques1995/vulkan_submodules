6a1985e6b84163c0a088f15569adbc9482453701
B Marques <brunosmarques1995@hotmail.com>
2023-08-23 21:39:10 -0300

    Added Release compile to validation layers

--- installers/validation.bat
+++ installers/validation.bat
@@ -2,5 +2,5 @@ set CompileMode=%~1
 
 cmake -S ./modules/robin_hood_hashing -B ./dependencies/robin_hood_hashing -DCMAKE_INSTALL_PREFIX="./install"
 cmake --build ./dependencies/robin_hood_hashing --config %CompileMode% --target install
-cmake -S ./modules/vulkan_validation -B ./dependencies/vulkan_validation -DCMAKE_INSTALL_PREFIX="./install" -DVVL_CODEGEN=ON -DUPDATE_DEPS=ON
-cmake --build ./dependencies/vulkan_validation --config Debug --target install
+cmake -S ./modules/vulkan_validation -B ./dependencies/vulkan_validation -DCMAKE_INSTALL_PREFIX="./install" -DVVL_CODEGEN=ON -DUPDATE_DEPS=ON -DCMAKE_BUILD_TYPE=%CompileMode%
+cmake --build ./dependencies/vulkan_validation --config %CompileMode% --target install
